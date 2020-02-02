5747a6ac81d47187f1a66716d2408cb3e8f722eb
Evgeny Terekhin <eter55@yandex.ru>
2020-02-02 19:43:30 +0300

    init

---
+++ GcHandleTests/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+namespace GcHandleTests
+{
+    class Program
+    {
+        private static object[] _ints;
+        private static object _o;
+
+        static void Main(string[] args)
+        {
+            _o = new Object().GCWatch("My Object created at " + DateTime.Now);
+            GC.Collect(0);
+            GC.KeepAlive(_o);
+            _o = null;
+            GC.Collect(0);
+            Console.ReadLine();
+        }
+    }
+
+    internal static class GCWatcher
+    {
+        private static readonly ConditionalWeakTable<Object,
+            NotifyWhenGCd<String>> s_cwt =
+            new ConditionalWeakTable<Object, NotifyWhenGCd<String>>();
+
+        private sealed class NotifyWhenGCd<T>
+        {
+            private readonly T m_value;
+
+            internal NotifyWhenGCd(T value)
+            {
+                m_value = value;
+            }
+
+            public override string ToString()
+            {
+                return m_value.ToString();
+            }
+
+            ~NotifyWhenGCd()
+            {
+                Console.WriteLine("GC'd: " + m_value);
+            }
+        }
+
+        public static T GCWatch<T>(this T @object, String tag) where T : class
+        {
+            s_cwt.Add(@object, new NotifyWhenGCd<String>(tag));
+            return @object;
+        }
+    }
+
+    public delegate bool CallBack(int handle, IntPtr param);
+
+    internal static class NativeMethods
+    {
+        [DllImport("Dll2.dll")]
+        public static extern ulong fibonacci_current();
+
+        [DllImport("Dll2.dll")]
+        public static extern void fibonacci_init(ulong a, ulong b);
+
+        [DllImport("Dll2.dll")]
+        public static extern bool fibonacci_next();
+
+        [DllImport("Dll2.dll")]
+        public static extern unsafe int* initialArray(int* arr, int length);
+    }
+}
---
+++ Kimedics.MemoryTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Kimedics.MemoryTests
+{
+    public class ICallForTest1Tests
+    {
+        private HandleCollector _handleCollector;
+
+        public ICallForTest1Tests()
+        {
+            _handleCollector = new HandleCollector("call", 1, 10);
+        }
+
+        public void A() => _handleCollector.Add();
+        public void B() => _handleCollector.Remove();
+    }
+
+    public class NotepadWriter
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private Process _process;
+
+        private static HandleCollector _handleCollector
+            = new HandleCollector("NotepadWriter", 1, 10);
+
+        public NotepadWriter(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _handleCollector.Add();
+            _process = Process.Start(@"C:\\Windows\\System32\\notepad.exe");
+        }
+
+        ~NotepadWriter()
+        {
+            _handleCollector.Remove();
+            _testOutputHelper.WriteLine($"GC, collect");
+            _process.Kill();
+        }
+    }
+
+  
+
+
+    public class UnitTest1
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
